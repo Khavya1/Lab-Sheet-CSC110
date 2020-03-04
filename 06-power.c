284af15974df0d631aec43dc960f6a6011c1619d
Anukul Adhiakri <beinganukul@gmail.com>
2020-03-04 19:11:54 +0545

    FINISHED LAB SHEET 4

---
+++ 06-power.c
@@ -0,0 +1,19 @@
+// compiling doenst work properly without linking math library so 
+// use the following syntax only for gcc users
+// gcc -o file_name file_name.c -lm
+#include<stdio.h>
+#include<math.h>
+
+int main() {
+	int a,b,c;
+	printf("Enter two numbers first being base and second being power");
+	scanf("%d%d",&a,&b);
+	c = p(a,b);
+	printf("The value is %d",c);
+}
+int p(int a,int b){
+	int po;
+	po = pow(a,b);
+	return po;
+}
+
---
+++ 12-sum-of-natural-numbers.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+int main() {
+	int b;
+	b = sum(20);
+	printf("The sum is %d",b);
+}
+int sum(int a){
+	int i,sum = 0;
+	for(i=1;i<=a;i++){
+		sum = sum+i;
+	}
+	return sum;
+}
+
+	
